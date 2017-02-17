ffaafd44d8b227e28ae91cd6a1b25ee2aa22705a
Bayin <bayin@ppdai.com>
2017-02-17 17:27:34 +0800

    add plus and sqrt function

--- src/main/java/com/paicaifu/app/web/HelloController.java
+++ src/main/java/com/paicaifu/app/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.paicaifu.app.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    public String index(@RequestParam("a") int a, @RequestParam("b") int b) {
+        int c = a + b;
+        return String.valueOf(a) + "+" + String.valueOf(b) + "=" + String.valueOf(c);
+    }
+
+    @RequestMapping("/sqrt")
+    public String sqrt(@RequestParam("a") String a) {
+        double result, temp;
+        try {
+            temp = Double.parseDouble(a);
+        } catch (Exception e) {
+            return "参数传递错误: " + a;
+        }
+        if (Double.parseDouble(a) < 0) return "不能传负数: " + a;
+        result = Math.sqrt(temp);
+
+        return String.valueOf(result);
     }
 }
